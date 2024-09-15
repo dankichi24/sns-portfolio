3f789a7f6735ed8694a19056ec87fe9fcbf6c7ae
dankichi <dan24.yukito@gamil.com>
2024-09-15 20:31:24 +0900

    prismaの初期化セットアップ

--- api/server.js
+++ api/server.js
@@ -3,8 +3,4 @@ const app = express();
 
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-});
-
 app.listen(PORT, () => console.log(`server is running on Port ${PORT}`));
