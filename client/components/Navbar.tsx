258a6447b79d356ce772d0bfb482914b6393aeb2
dankichi <dan24.yukito@gamil.com>
2024-09-25 19:18:07 +0900

    最初の画面とsns用ナビゲーションバーの作成

---
+++ client/components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <header className="w-full bg-indigo-900 text-white py-4">
+      <div className="w-full flex justify-between items-center px-12">
+        <h1 className="text-2xl font-bold">Gaming Device Share</h1>
+        <div className="flex space-x-3">
+          <Link
+            href="/auth/signin"
+            className="bg-white text-indigo-900 px-4 py-2 rounded font-bold hover:bg-gray-200 transition duration-300"
+          >
+            ログイン
+          </Link>
+          <Link
+            href="/auth/signup"
+            className="bg-white text-indigo-900 px-4 py-2 rounded font-bold hover:bg-gray-200 transition duration-300"
+          >
+            新規登録
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
