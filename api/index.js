7208f6e5471b7b747961488045c1067b75f0ee40
dankichi <dan24.yukito@gamil.com>
2025-04-12 21:17:16 +0900

    修正2

--- api/index.js
+++ api/index.js
@@ -16,13 +16,7 @@ if (process.env.FRONTEND_URL) {
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
