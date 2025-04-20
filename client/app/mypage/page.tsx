f9a4a1a29933ac25879cb7e20a3e854d52016106
dankichi <dan24.yukito@gamil.com>
2025-04-20 21:23:10 +0900

    マイページをサーバー＆クライアントコンポーネントに分離

--- client/app/mypage/page.tsx
+++ client/app/mypage/page.tsx
@@ -1,67 +1,7 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { useAuth } from "@/lib/authContext";
-import { useRouter, useSearchParams } from "next/navigation";
-import ShareHistory from "@/components/profile/ShareHistory";
-import ShareFavorites from "@/components/profile/ShareFavorites";
-import Profile from "@/components/profile/Profile";
+import MyPageClient from "./MyPageClient";
 
 const MyPage = () => {
-  const { user } = useAuth();
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const [activeTab, setActiveTab] = useState("profile");
-
-  useEffect(() => {
-    const tab = searchParams.get("activeTab");
-    if (tab) setActiveTab(tab);
-  }, [searchParams]);
-
-  useEffect(() => {
-    if (!user) {
-      router.push("/auth/signin");
-    }
-  }, [user, router]);
-
-  if (!user) return null;
-
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <div className="max-w-4xl mx-auto p-8 bg-white shadow-lg rounded-md mt-6">
-        <div className="flex justify-center space-x-8 border-b pb-4">
-          {["profile", "history", "favorites"].map((tab) => (
-            <button
-              key={tab}
-              className={`text-lg font-bold ${
-                activeTab === tab
-                  ? "text-indigo-900 border-b-2 border-indigo-900"
-                  : "text-gray-500"
-              }`}
-              onClick={() => setActiveTab(tab)}
-            >
-              {tab === "profile"
-                ? "プロフィール"
-                : tab === "history"
-                ? "Share履歴"
-                : "お気に入り"}
-            </button>
-          ))}
-        </div>
-
-        <div className="mt-6">
-          {activeTab === "profile" && <Profile />}
-          {activeTab === "history" && (
-            <ShareHistory userId={user.userId} active />
-          )}
-          {activeTab === "favorites" && (
-            <ShareFavorites userId={user.userId} active />
-          )}
-        </div>
-      </div>
-    </div>
-  );
+  return <MyPageClient />;
 };
 
 export default MyPage;
